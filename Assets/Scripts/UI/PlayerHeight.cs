699f3c1c531ebef1d2e3d814dcacec08b5458d6c
Damian Mendez <kiliranshe@hotmail.com>
2021-08-28 12:42:46 +0200

    Funcion para mostrar la altura del avion

---
+++ Assets/Scripts/UI/PlayerHeight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class PlayerHeight : MonoBehaviour
+    {
+        [SerializeField]
+        private Text currentPlayerHeight;
+        [SerializeField]
+        private GameObject initialHeightPosition;
+        [SerializeField]
+        private GameObject EndingLevelPosition;
+
+        private void Awake()
+        {
+            initialHeightPosition = initialHeightPosition;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            UpdateCurrentHeight();
+        }
+
+        public void UpdateCurrentHeight()
+        {
+            currentPlayerHeight.text = GameManager.instance.mTool.MeasureHigh(GameManager.instance.player.transform.position,
+                initialHeightPosition.transform.position).ToString();
+        }
+    }
+}
