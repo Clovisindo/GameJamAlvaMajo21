8014652429dc5dc4e8daa073dcbe15b407a8c5f8
Damian Mendez <kiliranshe@hotmail.com>
2021-08-31 19:19:25 +0200

    Creada escena de menu inicio

---
+++ Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+	public class MenuScript : MonoBehaviour
+	{
+		public void triggerMenu(int trigger)
+		{
+			switch (trigger)
+			{
+				case (0):
+					SceneManager.LoadScene("gameScene");
+					break;
+				case (1):
+					Application.Quit();
+					break;
+			}
+		}
+	}
+}
